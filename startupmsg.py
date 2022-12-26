98fed3afbfeea5d6bb6f646f8b772facf540a878
Daniel Serrano <daniel.serrano@secuoyas.com>
2022-12-26 20:09:47 +0100

    fix

--- startupmsg.py
+++ startupmsg.py
@@ -78,11 +78,6 @@ else:
     solar_noon = "12:00:00"
     sunset = "20:00:00"
 
-print("_,-._")
-print("/ \_/ \")
-print(">-(_)-<")
-print("\_/ \_/")
-print(" `-' ")  
 # Gets the greeting message ready
 # Night => bright blue "[94m
 # Morning => bright yellow "[93m
